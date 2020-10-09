ad93d64da5b8055a12fff590657e9b4d41f61e42
natan barros <natanbarros133@gmail.com>
2020-10-09 00:19:38 -0300

    first commit

--- Startup.cs
+++ Startup.cs
@@ -72,10 +72,10 @@ namespace Shop
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
+            // if (env.IsDevelopment())
+            // {
                 app.UseDeveloperExceptionPage();
-            }
+            // }
 
             app.UseHttpsRedirection();
             
